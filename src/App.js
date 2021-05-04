d173c78c8a3e449634d385399358b2e37bfbdd97
Marijke <marijkemeersman@gmail.com>
2021-05-04 21:55:27 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello world!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +19,29 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+      Inside that directory, you can run several commands:
+
+npm start
+  Starts the development server.
+
+npm run build
+  Bundles the app into static files for production.
+
+npm test
+  Starts the test runner.
+
+npm run eject
+  Removes this tool and copies build dependencies, configuration files
+  and scripts into the app directory. If you do this, you can’t go back!
+
+We suggest that you begin by typing:
+
+cd my-first-react-app
+npm start
+
+Happy hacking!
+      </body>
     </div>
   );
 }
